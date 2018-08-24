08672617d4aeb19f822e53c8ec90577e5587cbdc
crellmonf <anton.gumiran@gmail.com>
2018-08-24 10:11:05 +0800

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=anton-chat-bot-csharp-Web-Deploy.pubxml -p:Password=bZSZJtaK1FoAK4zlbJEHd8DEkWXSAm02nvhmLckqnoWcbE9yPmnKRTkP94yB
+
